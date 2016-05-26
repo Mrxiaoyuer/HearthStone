0ec818c4f83d2ba598541d159052058397cfee65
Wang Yong-chao <chaowyc@hit.edu.cn>
2016-05-26 15:52:52 +0800

    fixed same bug

--- Manager/AssignList.php
+++ Manager/AssignList.php
@@ -23,8 +23,8 @@
 		while($now = $ans->fetch_assoc()){
 			if($now["Assigned"] == 0)
 			{
-				$Dep_No = 1;
-				$Pat_id = $now["Pat_ID"];
+				$Dep_No = $now["Dep_No"];
+				$Pat_ID = $now["Pat_ID"];
 				$Assigned = $now["Assigned"]; 
 				echo "<tr>";
 				echo "<td>" . $now["Pat_name"] . "</td>";
@@ -32,10 +32,35 @@
 				echo "<td>" . $now["Primary_doc"] . "</td>";
 				echo "<td>" . $now["Dep_name"] . "</td>";
 				echo "<tr>";
+				echo "</table>";
+
+				echo '<table class="table">';
+				echo "<br><h3 class = 'text-center'>Empty Room List</h3><br><br>";
+				echo "<th>" . "Room_Num" . "<br >". "</th>";
+				echo "<th>" . "Bed_Num" . "<br >". "</th>";
+				echo "<th>" . "Department" . "<br >". "</th>";
+				echo "<th>" . "Opreation" . "<br >". "</th>";
+				$bns = $con->query("select * from Bed natural join Room, Department where Belong_dep = Dep_No and Belong_room = Room_No and Dep_No = $Dep_No and Is_used = 0;");
+				while ($room = $bns->fetch_assoc()){
+        				echo "<tr>";
+        				echo "<td>" . $room["Belong_room"] . "</td>";
+        				echo "<td>" . $room["inroom_id"] . "</td>";
+        				echo "<td>" . $room["Dep_name"]. "</td>";
+        				echo "<td>" . "<a href='_AssignAction.php?id1=$room[Bed_No]&id2=$Pat_id' class='btn btn-info'>Assign</a>" . "</td>";
+        				echo "</tr>";
+    					
+    				}
+    				$bns->close();
+    				//$ans = $con->query("select * from Patient natural join Doctor, Department where Work_at = Dep_No and Primary_doc = Work_ID");
 			}
+			echo "</table>";
+			echo '<table class="table">';
+			echo "<br><h3 class = 'text-center'>Pending Patient</h3><br><br>";
+			echo "<th>" . "Pat_name" . "<br >". "</th>";
+			echo "<th>" . "Sex" . "<br >". "</th>";
+			echo "<th>" . "Primary_doc" . "<br >". "</th>";
+			echo "<th>" . "Department" . "<br >". "</th>";
 		}
-			
-		
 		$ans->close();
 	  ?>
 	</table>
