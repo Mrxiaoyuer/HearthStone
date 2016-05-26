77b75683a70d65de189a81db01bab9ac76c4d807
Mrxiaoyuer <a1021843147@qq.com>
2016-05-26 14:58:56 +0800

    Not verified User's initial Pages Done

--- index.php
+++ index.php
@@ -8,19 +8,22 @@
     header("location:Manager/ManageMain.php");
   }
 ?>
-<?php 
+<?php
   require('header.php');        // 导航栏
 ?>
 
     <div class="container">
       <h1 class = "text-center">Hospital Logistics System</h1>
       <br>
-      <div class="form-signin">
-        <div class="alert alert-success">You have been <strong>successfully logged in</strong>.</div>
-        <a href="login/logout.php" class="btn btn-default btn-lg btn-block">Logout</a>
+      <div class="col-md-offset-2 col-md-8">
+
+        <div class="jumbotron">
+          <div class="alert alert-success text-center">You have been successfully logged in.</div>
+          &nbsp &nbsp If this is your first time to sign in, please be patient and wait for manager's Authenticatation to obtain full access . . . .
+        </div>
       </div>
     </div> <!-- /container -->
 
-<?php 
+<?php
   require('footer.php');        // 底部
-?> 
+?>
